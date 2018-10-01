518f44887a9d81413e27e949da519a1a1cd73ac7
Geovany <geovanyscv@gmail.com>
2018-09-30 22:45:18 -0300

    Adicionando comentarios

--- prices.py
+++ prices.py
@@ -24,19 +24,22 @@ class PricesDriver(object):
         text_element = self.driver.find_element_by_xpath("//h3[text() = 'Bare Metal']"
                                                          "/parent::div"
                                                          "/parent::div[@class='row']")
+        """ Pegando o select de seleção de localização """
         options_localization, select_localization = self.get_options_localization(text_element)
 
         for option_localization in options_localization:
             data[option_localization.text] = []
 
+            """ Selecionando uma opção"""
             select_localization.select_by_value(option_localization.get_attribute("value"))
             trs = text_element.find_elements_by_tag_name("tr")
 
             for tr in trs:
                 tds = tr.find_elements_by_tag_name("td")
+
+                """ Verificando se é td(valores) ou th(titulos) """
                 if len(tds) > 0:
-                    columns = {}
-                    index = 0
+                    columns, index = {}, 0
 
                     for td in tds:
                         text_th = titles[index]
@@ -53,6 +56,7 @@ class PricesDriver(object):
                         text_th = th.text
                         titles.append(text_th)
 
+        """ Convertendo para JSON e imprimindo na tela """
         data_json = json.dumps(data, indent=4)
         pprint(data_json)
         self.driver.close()
