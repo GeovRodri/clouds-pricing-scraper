945242794bae90cecf347ba61e466a625cc9d432
Geovany <geovanyscv@gmail.com>
2018-11-04 19:21:56 -0200

    Criando api da azure

--- azure/azure_api.py
+++ azure/azure_api.py
@@ -1,11 +1,18 @@
-import csv
-import json
+import configparser
+import adal
+import os
 import requests
+from azure.mgmt.commerce.models import RateCardQueryParameters
+from msrestazure.azure_active_directory import AADTokenCredentials
 
 
 class Prices(object):
 
     def get_price(self):
+        currency = 'USD'
+        locale = 'en-US'
+        region = 'US'
+
         locations = []
         locations_keys = []
 
@@ -15,70 +22,22 @@ class Prices(object):
         headers = ['Size', 'Clock speed (ACU/Core)', 'CPU cores;Memory (GiB)', 'Local HDD (GiB)', 'NICs (Max)',
                    'Network bandwidth']
 
-        # url = 'https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/index.json'
-        # json_prices = requests.get(url)
-        json_prices = open('aws.json').read()
+        authority_host_uri = 'https://login.microsoftonline.com'
+        tenant = '85bf2e1d-3d45-4132-9d91-c35396e877d4'
+        authority_uri = authority_host_uri + '/' + tenant
+        resource_uri = 'https://management.core.windows.net/'
+        client_id = '34688905-7185-496b-9027-f6e172322810'
 
-        all_prices = json.loads(json_prices)
-        products = all_prices['products']
-        on_demand = all_prices['terms']['OnDemand']
+        context = adal.AuthenticationContext(authority_uri, api_version=None)
+        code = context.acquire_user_code(resource_uri, client_id)
+        print(code['message'])
+        mgmt_token = context.acquire_token_with_device_code(resource_uri, code, client_id)
+        credentials = AADTokenCredentials(mgmt_token, client_id)
 
-        for product_key in products:
-            product = products[product_key]
-
-            sku = product['sku']
-            location = product['attributes'].get('location', None)
-            if location is None:
-                continue
-
-            if location not in locations_keys:
-                locations.append({'Name': location})
-                locations_keys.append(location)
-
-            instance_type = product['attributes'].get('instanceType', None)
-
-            if instance_type not in instances_keys:
-                obj = {
-                    'Instance Type': product['attributes'].get('instanceType', None),
-                    'GPUs': instance_type,
-                    'vCPU': product['attributes'].get('vcpu', None),
-                    'Memory (GiB)': product['attributes'].get('memory', None),
-                    'Storage (GB)': product['attributes'].get('storage', None),
-                    'Networking Performance': product['attributes'].get('networkPerformance', None),
-                    'Physical Processor': product['attributes'].get('physicalProcessor', None),
-                    'Clock Speed (GHz)': product['attributes'].get('clockSpeed', None),
-                    'Intel AVX': product['attributes'].get('intelAvxAvailable', '-'),
-                    'Intel AVX2': product['attributes'].get('intelAvx2Available', '-'),
-                    'Intel Turbo': product['attributes'].get('intelTurboAvailable', '-'),
-                    'EBS OPT': product['attributes'].get('ebsOptimized', '-'),
-                    'Enhanced Network': product['attributes'].get('enhancedNetworkingSupported', '-')
-                }
-
-                instances.append(obj)
-                instances_keys.append(instance_type)
-            else:
-                index = instances_keys.index(instance_type)
-                obj = instances[index]
-
-            offers = on_demand.get(sku, None)
-
-            if offers is not None:
-                offers_keys = list(offers.keys())
-
-                dimensions = offers[offers_keys[0]]['priceDimensions']
-                dimensions_keys = list(dimensions.keys())
-
-                obj[location] = dimensions[dimensions_keys[0]]['pricePerUnit']['USD']
-
-        with open('AWS_Regions.csv', 'w') as f:  # Just use 'w' mode in 3.x
-            w = csv.DictWriter(f, fieldnames=list(locations[0].keys()), delimiter=';')
-            w.writeheader()
-            w.writerows(locations)
-
-        with open('AWS.csv', 'w') as f:  # Just use 'w' mode in 3.x
-            w = csv.DictWriter(f, fieldnames=headers + locations_keys, delimiter=';')
-            w.writeheader()
-            w.writerows(instances)
+        url = "https://management.azure.com:443/subscriptions/2bed4f87-fd56-40e9-9afa-27446d7856d2/providers/Microsoft.Commerce/RateCard?api-version=2016-08-31-preview&$filter=Currency eq '{currencyId}' and Locale eq '{localeId}' and RegionInfo eq '{regionId}'"\
+            .format(currencyId=currency, localeId=locale, regionId=region)
+        json_prices = requests.get(url, allow_redirects=False, headers={'Authorization': 'Bearer {}'.format(credentials.token['access_token'])})
+        print(json_prices)
 
 
 if __name__ == "__main__":
