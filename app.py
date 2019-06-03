a71a6b4cc11e764e81feb5cdf56afea9d90f4039
geovany <geovanyscv@gmail.com>
2019-06-03 11:39:42 -0300

    Alterando retorno dos itens

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ class Prices(Resource):
             last_data = list(mongo.find_last(collection_name))[0]
             fields = form.data['select'].get(collection_name, [])
             filters = form.data['filters'].get(collection_name, [])
-            items = {}
+            items = []
 
             if fields is None or len(fields) == 0:
                 result[collection_name] = {}
@@ -37,9 +37,9 @@ class Prices(Resource):
 
             del last_data['_id']
             for machine_key in last_data:
-                remove_machine = False
                 machine = last_data[machine_key]
-                item, general_configurations, price_configurations = {}, {}, []
+                price_configurations = []
+                general_configurations = {'type': machine_key}
 
                 """ Montando o objeto """
                 for field_list in fields:
@@ -49,29 +49,28 @@ class Prices(Resource):
 
                     if 'pricing' in field:
                         price_configurations = self.extract_pricing(field_data, field_split[1])
-
-                item = [{**general_configurations, **x} for x in price_configurations]
-
-                """ Realizando a filtragem dos resultados """
-                for filter_obj in filters:
-                    field_filter = filter_obj["field"]
-                    comparator = filter_obj["comparator"]
-                    value_filter = filter_obj["value"]
-
-                    field_data = "'" + item.get(field_filter, None) + "'"
-
-                    # Removendo . caso seja um número decimal
-                    if str(value_filter).replace('.', '').isnumeric():
-                        field_data = Utils.only_numbers(field_data)
                     else:
-                        # Se não for um número, adicionar " para considerar como string na hora do eval
-                        value_filter = '"' + value_filter + '"'
+                        general_configurations[field] = field_data
 
-                    if eval("{} {} {}".format(field_data, comparator, value_filter)) is False:
-                        remove_machine = True
+                items = items + [{**general_configurations, **x} for x in price_configurations]
 
-                if remove_machine is False:
-                    items[machine_key] = item
+            """ Realizando a filtragem dos resultados """
+            for filter_obj in filters:
+                field_filter = filter_obj["field"]
+                comparator = filter_obj["comparator"]
+                value_filter = filter_obj["value"]
+
+                field_data = "'" + item.get(field_filter, None) + "'"
+
+                # Removendo . caso seja um número decimal
+                if str(value_filter).replace('.', '').isnumeric():
+                    field_data = Utils.only_numbers(field_data)
+                else:
+                    # Se não for um número, adicionar " para considerar como string na hora do eval
+                    value_filter = '"' + value_filter + '"'
+
+                if eval("{} {} {}".format(field_data, comparator, value_filter)) is False:
+                    remove_machine = True
 
             result[collection_name] = items
 
